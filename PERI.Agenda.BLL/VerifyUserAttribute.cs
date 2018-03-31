d63ef327beb0a5969d47e2697cfceb238fddf9f0
Alvin Chua <actchua@periapsys.com>
2018-03-31 18:16:52 +0800

    Fetch User from ActionFilter

--- PERI.Agenda.BLL/VerifyUserAttribute.cs
+++ PERI.Agenda.BLL/VerifyUserAttribute.cs
@@ -31,6 +31,22 @@ namespace PERI.Agenda.BLL
             try
             {
                 var payload = decoder.DecodeToObject<Dictionary<string, object>>(authHeader, secret, verify: true);
+                var userId = Convert.ToInt32(payload["id"]);
+
+                // Verifiy user
+                var context = new EF.AARSContext();
+                var bll_u = new BLL.EndUser(context);
+                var user = bll_u.Get(new EF.EndUser { UserId = userId }).Result;
+
+                if (user.DateInactive != null)
+                {
+                    logger.Error("Unauthorized.");
+                    filterContext.Result = new UnauthorizedResult();
+                }
+                else
+                {
+                    filterContext.HttpContext.Items.Add("EndUser", user);
+                }
             }
             catch (TokenExpiredException ex)
             {
