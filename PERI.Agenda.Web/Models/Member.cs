9d9477d1eb42b329e973a130590738c58f06bdc7
Alvin Chua <actchua@periapsys.com>
2018-04-23 07:54:08 +0800

    Add model

---
+++ PERI.Agenda.Web/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PERI.Agenda.Web.Models
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
