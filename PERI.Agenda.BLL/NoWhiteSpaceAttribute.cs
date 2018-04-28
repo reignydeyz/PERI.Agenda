d7de96a2933e73083763b6cbe83ff4f9fbe5452a
Alvin Chua <actchua@periapsys.com>
2018-04-28 10:52:55 +0800

    WhiteSpace validator

---
+++ PERI.Agenda.BLL/NoWhiteSpaceAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PERI.Agenda.BLL
+{
+    /// <summary>
+    /// Custom Validator - Whitespace validator for non-required field
+    /// </summary>
+    public class NoWhiteSpaceAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && value.GetType() == typeof(string))
+            {
+                var str = value as string;
+                
+                if (str.Length > 0)
+                {
+                    if (String.IsNullOrWhiteSpace(str)) {
+                        return new ValidationResult(validationContext.DisplayName + " value is just whitespace");
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
