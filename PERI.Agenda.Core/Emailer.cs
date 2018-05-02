88bc4fe19e998d7d746cf37d1e53f08a52bafeea
Alvin Chua <actchua@periapsys.com>
2018-05-02 22:04:51 +0800

    SmtpClient to Emailer

---
+++ PERI.Agenda.Core/Emailer.cs
@@ -0,0 +1,43 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PERI.Agenda.Core
+{
+    public class Emailer
+    {
+        public string DisplayName { get; set; }
+        public string DisplayEmail { get; set; }
+        public string SmtpServer { get; set; }
+        public string SmtpPort { get; set; }
+        public bool UseSsl { get; set; }
+        public string SmtpUser { get; set; }
+        public string SmtpPassword { get; set; }
+
+        public async Task SendEmail(string to, string subject, string message)
+        {
+            var msg = new MimeMessage();
+
+            msg.From.Add(new MailboxAddress(DisplayName, DisplayEmail));
+            msg.To.Add(new MailboxAddress("", to));
+            msg.Subject = subject;
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = message;
+            msg.Body = bodyBuilder.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(SmtpServer, Convert.ToInt32(SmtpPort), Convert.ToBoolean(UseSsl));
+
+                // Note: only needed if the SMTP server requires authentication
+                await client.AuthenticateAsync(SmtpUser, SmtpPassword);
+
+                await client.SendAsync(msg);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
