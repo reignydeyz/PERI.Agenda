e2db9db5d2bf9d036fecfcb53cb37968dddd9a87
Alvin Chua <actchua@periapsys.com>
2018-06-02 23:05:46 +0800

    Fix cookie issue

--- PERI.Agenda.Web/ClientApp/app/components/dashboard/dashboard.component.ts
+++ PERI.Agenda.Web/ClientApp/app/components/dashboard/dashboard.component.ts
@@ -57,7 +57,7 @@ export class DashboardComponent {
             else {
                 this.onAttendanceLoad();
             }
-        });
+        }, error => this.am.ex.catchError(error));
     }
 
     onAttendanceLoad() {
