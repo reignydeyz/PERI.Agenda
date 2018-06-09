b4e12e21a64b03889e392d74df2e91011f6c37b0
ERNI\chal <alvin.chua@erni.ph>
2018-06-09 14:26:23 +0800

    Repository Pattern

---
+++ PERI.Agenda.Repository/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PERI.Agenda.Repository
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly EF.AARSContext _dbContext;
+        private DbSet<T> _dbSet => _dbContext.Set<T>();
+        public IQueryable<T> Entities => _dbSet;
+        public GenericRepository(EF.AARSContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _dbSet.AddRange(entities);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+    }
+}
---
+++ PERI.Agenda.Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PERI.Agenda.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> Entities { get; }
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Add(T entity);
+        Task AddAsync(T entity);
+        void AddRange(IEnumerable<T> entities);
+        Task AddRangeAsync(IEnumerable<T> entities);
+    }
+}
