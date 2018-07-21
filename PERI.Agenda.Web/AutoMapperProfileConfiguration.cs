26afc0098a3f54a54aec6700c171b1c04e868905
Alvin Chua <actchua@periapsys.com>
2018-07-21 20:35:59 +0800

    Fix sign-up issue

--- PERI.Agenda.Web/AutoMapperProfileConfiguration.cs
+++ PERI.Agenda.Web/AutoMapperProfileConfiguration.cs
@@ -11,6 +11,8 @@ namespace PERI.Agenda.Web
         public AutoMapperProfileConfiguration()
         {
             CreateMap<Models.Member, EF.Member>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<Models.SignUp, EF.Member>().IgnoreAllPropertiesWithAnInaccessibleSetter();
+
             CreateMap<Models.Event, EF.Event>().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<Models.EventCategory, EF.EventCategory>().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<Models.Location, EF.Location>().IgnoreAllPropertiesWithAnInaccessibleSetter();
