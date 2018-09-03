d87166626d82cfdbb875e83a3b560f8020e486e5
ERNI\chal <alvin.chua@erni.ph>
2018-09-03 15:56:09 +0800

    ActivityReport

--- PERI.Agenda.Web/ClientApp/app/components/activityreport/activityreport.component.ts
+++ PERI.Agenda.Web/ClientApp/app/components/activityreport/activityreport.component.ts
@@ -6,6 +6,7 @@ import { Title } from '@angular/platform-browser';
 import { ErrorExceptionModule } from '../errorexception/errorexception.component';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
+import { Report, ReportModule } from '../reporttemplate/reporttemplate.component';
 
 @Component({
     selector: 'activityreport',
@@ -15,9 +16,23 @@ export class ActivityReportComponent {
     @Input() groupId: number = 0;
 
     private ex: ErrorExceptionModule;
+    private rm: ReportModule;
+
+    reports: Report[] = [];
 
     constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string, private titleService: Title) {
         this.ex = new ErrorExceptionModule();
+        this.ex.http = http;
         this.ex.baseUrl = baseUrl;
+
+        this.rm = new ReportModule();
+        this.rm.baseUrl = baseUrl;
+        this.rm.http = http;
     } 
+
+    ngOnInit() {
+        this.rm.find(new Report()).subscribe(r => {
+            this.reports = r;
+        });
+    }
 }
