0654c689168a27f7cfe2200d3ea8c2f4d3506866
Alvin Chua <actchua@periapsys.com>
2019-03-15 07:51:25 +0800

    Moq Callback

--- PERI.Agenda.Test/MemberTests.cs
+++ PERI.Agenda.Test/MemberTests.cs
@@ -4,126 +4,85 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace PERI.Agenda.Test
 {
     public class MemberTests
     {
-        /*private readonly EF.AARSContext context;
-        private readonly BLL.UnitOfWork unitOfWork;
+        private Mock<BLL.IUnitOfWork> mockUnitOfWork;
 
-        public MemberTests()
-        {
-            TestHelper.GetApplicationConfiguration();
-            context = new EF.AARSContext();
-            unitOfWork = new BLL.UnitOfWork(context);
-        }
+        private readonly BLL.IMember memberBusiness;
 
-        [Theory]
-        [MemberData(nameof(TestDataGenerator.FindMember_HasResultParams), MemberType = typeof(TestDataGenerator))]
-        public void FindMember_HasResult(EF.Member args)
-        {
-            var bll_m = new BLL.Member(unitOfWork);
-            var bll_lu = new BLL.LookUp(unitOfWork);
-
-            var members = from r in bll_m.Find(args).ToList()
-                        join genders in bll_lu.GetByGroup("Gender").Result.Select(x => new { Label = x.Name, Value = int.Parse(x.Value) }) on r.Gender equals genders.Value into e
-                            from e1 in e.DefaultIfEmpty()
-                        join civilStatuses in bll_lu.GetByGroup("Civil Status").Result.Select(x => new { Label = x.Name, Value = int.Parse(x.Value) }) on r.CivilStatus equals civilStatuses.Value into f
-                            from f1 in f.DefaultIfEmpty()
-                        join m in bll_m.Find(new EF.Member { CommunityId = 1 }) on r.InvitedBy equals m.Id into g
-                            from m1 in g.DefaultIfEmpty()
-                        select new
-                        {
-                            r.Id,
-                            r.Name,
-                            r.NickName,
-                            r.Address,
-                            r.Mobile,
-                            r.Email,
-                            r.BirthDate,
-                            r.Remarks,
-                            CivilStatus = f1 == null ? "" : f1.Label,
-                            Gender = e1 == null ? "" : e1.Label,
-                            InvitedBy = m1 == null ? "" : m1.Name,
-                            r.DateCreated
-                        };
-
-            Assert.True(members.Count() > 0);
-        }
-
-        [Theory]
-        [MemberData(nameof(TestDataGenerator.FindMember_HasNoResultParams), MemberType = typeof(TestDataGenerator))]
-        public void FindMember_HasNoResult(EF.Member args)
-        {
-            var bll_m = new BLL.Member(unitOfWork);
-
-            var members = bll_m.Find(args).ToList();
-
-            Assert.True(members.Count == 0);
-        }
-
-        [Theory]
-        [MemberData(nameof(TestDataGenerator.AddMember_SuccessParams), MemberType = typeof(TestDataGenerator))]
-        public void AddMember_Success(EF.Member args)
-        {
-            var bll_m = new BLL.Member(unitOfWork);
-
-            var id = bll_m.Add(args).Result;
-
-            Assert.True(id > 0);
-        }*/
-
-        private readonly Mock<BLL.IUnitOfWork> mockUnitOfWork;
+        private Mock<Repository.IRepository<EF.Member>> mockMemberRepo;
 
         public MemberTests()
         {
             mockUnitOfWork = new Mock<BLL.IUnitOfWork>();
+
+            memberBusiness = new BLL.Member(mockUnitOfWork.Object);
+
+            mockMemberRepo = new Mock<Repository.IRepository<EF.Member>>();
+            mockMemberRepo.Setup(x => x.Entities).Returns(TestRepo.Members.AsQueryable());
+
+            mockUnitOfWork.Setup(x => x.MemberRepository).Returns(mockMemberRepo.Object);
         }
 
         [Theory]
         [MemberData(nameof(TestDataGenerator.FindMember_HasResultParams), MemberType = typeof(TestDataGenerator))]
         public void FindMember_HasResult(EF.Member args)
-        {
-            // Arrange
-            var mockMemberRepo = new Mock<Repository.IRepository<EF.Member>>();
-            mockMemberRepo.SetupGet(x => x.Entities).Returns(TestRepo.Members.AsQueryable());
-            mockUnitOfWork.SetupGet(x => x.MemberRepository).Returns(mockMemberRepo.Object);
+        {           
+            var res = memberBusiness.Find(args);
 
-            var mockMemberBusiness = new BLL.Member(mockUnitOfWork.Object);
-
-            // Act
-            var res = mockMemberBusiness.Find(args);
-
-            // Assert
             Assert.True(res.Count() > 0);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestDataGenerator.FindMember_HasNoResultParams), MemberType = typeof(TestDataGenerator))]
+        public void FindMember_NoResult(EF.Member args)
+        {
+            var res = memberBusiness.Find(args);
+
+            Assert.True(res.Count() == 0);
         }
 
         [Theory]
         [MemberData(nameof(TestDataGenerator.AddMember_SuccessParams), MemberType = typeof(TestDataGenerator))]
         public void AddMember_Success(EF.Member args)
         {
-            // Arrange
-            var mockMemberRepo = new Mock<Repository.IRepository<EF.Member>>();
-            mockMemberRepo.SetupGet(x => x.Entities).Returns(TestRepo.Members.AsQueryable());
-            mockUnitOfWork.SetupGet(x => x.MemberRepository).Returns(mockMemberRepo.Object);
+            var list = TestRepo.Members;
+            var count = TestRepo.Members.Count;
 
-            /*var mockRoleRepo = new Mock<Repository.IRepository<EF.Role>>();
-            mockRoleRepo.SetupGet(x => x.Entities).Returns(TestRepo.Roles.AsQueryable());
-            mockUnitOfWork.SetupGet(x => x.RoleRepository).Returns(mockRoleRepo.Object);
+            mockUnitOfWork.Setup(x => x.Commit()).Callback(() =>
+            {
+                list.Add(args);
+            });
 
-            var mockEndUserRepo = new Mock<Repository.IRepository<EF.EndUser>>();
-            mockEndUserRepo.SetupGet(x => x.Entities).Returns(TestRepo.EndUsers.AsQueryable());
-            mockUnitOfWork.SetupGet(x => x.EndUserRepository).Returns(mockEndUserRepo.Object);*/
+            var id = memberBusiness.Add(args).Result;
 
-            var mockMemberBusiness = new BLL.Member(mockUnitOfWork.Object);
+            Assert.True(list.Count > count);
+        }
 
-            // Act
-            var id = mockMemberBusiness.Add(args).Result;
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        public void DeleteMember_Success(params int[] args)
+        {
+            var list = TestRepo.Members;
+            var count = TestRepo.Members.Count;
 
-            // Assert
-            Assert.True(id >= 0);
+            mockUnitOfWork.Setup(x => x.CommitAsync()).Callback(() =>
+            {
+                var objs = list.Where(x => args.Contains(x.Id)).ToList();
+                foreach (var obj in objs)
+                    list.Remove(obj);
+            });
+
+            memberBusiness.Delete(args);
+
+            Assert.True(list.Count < count);
         }
     }
 }
--- PERI.Agenda.Test/TestRepo.cs
+++ PERI.Agenda.Test/TestRepo.cs
@@ -6,7 +6,7 @@ namespace PERI.Agenda.Test
 {
     public class TestRepo
     {
-        public static IEnumerable<EF.Member> Members => new List<EF.Member>
+        public static List<EF.Member> Members => new List<EF.Member>
             {
                 new EF.Member { Id = 1, Name = "ALVIN", CommunityId = 1, EndUser = new EF.EndUser { RoleId = 1, MemberId = 1 } },
                 new EF.Member { Id = 2, Name = "CHUA", CommunityId = 1, EndUser = new EF.EndUser { RoleId = 1, MemberId = 2 } },
@@ -14,13 +14,13 @@ namespace PERI.Agenda.Test
                 new EF.Member { Id = 4, Name = "JOHN", CommunityId = 1 , EndUser =  new EF.EndUser { RoleId = 1, MemberId = 4 }}
             };
 
-        public static IEnumerable<EF.Role> Roles => new List<EF.Role>
+        public static List<EF.Role> Roles => new List<EF.Role>
             {
                 new EF.Role { RoleId = 1, Name = "Admin" },
                 new EF.Role { RoleId = 2, Name = "User" },
             };
 
-        public static IEnumerable<EF.EndUser> EndUsers = new List<EF.EndUser>
+        public static List<EF.EndUser> EndUsers = new List<EF.EndUser>
             {
                 new EF.EndUser { RoleId = 1, MemberId = 1 },
                 new EF.EndUser { RoleId = 1, MemberId = 2 },
