e9871539706c7112dc2ce1194833a001b2ef6b29
Alvin Chua <actchua@periapsys.com>
2020-06-04 14:19:16 +0800

    Response interceptor

--- PERI.Agenda.Web/ClientApp/app/interceptors/res.interceptor.ts
+++ PERI.Agenda.Web/ClientApp/app/interceptors/res.interceptor.ts
@@ -5,11 +5,24 @@ import {
     HttpEvent,
     HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 @Injectable()
 export class ResponseInterceptor implements HttpInterceptor {
-    constructor() { }
+    constructor(private router: Router) { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request);
+        return next.handle(request)
+            .pipe(catchError(err => {
+
+                switch (err.status) {
+                    case 412:
+                    case 417:
+                        location.replace('/authentication/signout');
+                        break;
+                }
+
+                return new Observable<never>();
+            }));
     }
 }
